e03de258d7cf7745f09c4951f13d7c310f91a0fa
renatão <paulo05renato@gmail.com>
2024-03-23 16:50:13 -0300

    config url and create workflows

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'https://serverest.dev/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
